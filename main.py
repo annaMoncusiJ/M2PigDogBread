dbd6207c28f37c10667b6c127bf01b53a3cabcca
aMoncusiJ <anna.moncusi@gmail.com>
2024-01-22 11:44:15 +0100

    canvi d'alguns comentaris

--- main.py
+++ main.py
@@ -12,42 +12,44 @@ import joblib
 import torch.nn.functional as F
 from interficieGrafica import *
 
-# Redimensionar todas las imágenes al mismo tamaño
-target_size = (128, 128)  # Cambia el tamaño según tus necesidades
-# Transformaciones para normalizar las imágenes y convertirlas a tensores de PyTorch
+# Variable de les mides en les que possarem totes les imatges
+target_size = (128, 128)
+
+# Normalitza les imatges i les converteix en tensors ded PyTorch
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
-# Ruta del modelo entrenado
+#Ruta del model entrenat i previament guardat
 modelo_svm_ruta = 'svm_model.pkl'
-#-------------------------------------
-# Ruta base para las carpetas de imágenes
-# url = "C:\\Users\\aleej\\Desktop\\IA BIG DATA\\M2\\M2 A5 ClasificadorDogPigBread\\M2DogPigBread\\"
-# La url s'agafa de "config.py"
-# Verificar si el modelo ya existe
+
+#Conprova si el model esta creat, en cas de que ho estigui es salta aquest pas, ja que tarda molt i així no cal que faci el mateix cada vegada
 if not os.path.exists(modelo_svm_ruta):
     print("Entrenando el modelo SVM y guardándolo...")
     
-    # Ruta de la imagen de entrada que deseas clasificar
+    #fi de ruta de les imatges que volem classificar. La variable url esta definidda a config.
     img_dir_pig = os.path.join(url, "Pig")
     img_dir_dog = os.path.join(url, "Dog")
     img_dir_bread = os.path.join(url, "Bread")
 
-    # Listas de nombres de archivos para cada categoría
+    # Edita el nom dels arxius per afegir-lis la categoria al devant
     img_list_pig = ["pig_" + img for img in os.listdir(img_dir_pig) if img.endswith(".png")]
     img_list_dog = ["dog_" + img for img in os.listdir(img_dir_dog) if img.endswith(".png")]
     img_list_bread = ["bread_" + img for img in os.listdir(img_dir_bread) if img.endswith(".png")]
 
-    # Lista combinada de nombres de archivos
+    # Afegim totes les imatges en una llista
     img_list = img_list_pig + img_list_dog + img_list_bread
 
+    # Cambiem l'ordre de la llista per un altre a l'atzar
     random.shuffle(img_list)
 
+    # Creació de les variables que contindran l'entrenament
     train_images = []
     train_labels = []
 
     for img in img_list:
+        # Separa el tipus del nom original de l'imatge
         type, img = img.split('_', 1)
 
+        # Segons el tipus, assigna un "label"
         if type == "pig":
             label = 0
             img_dir = img_dir_pig
@@ -58,6 +60,7 @@ if not os.path.exists(modelo_svm_ruta):
             label = 2
             img_dir = img_dir_bread
 
+        # processa les imatges
         img_path = os.path.join(img_dir, img)
         img_pil = Image.open(img_path).convert("L")
         img_tensor = transform(img_pil)
@@ -76,19 +79,20 @@ if not os.path.exists(modelo_svm_ruta):
         def _getitem_(self, idx):
             return self.images[idx], self.labels[idx]
 
+    # crea un dataset amb les imatges processades
     train_dataset = CustomDataset(train_images, train_labels)
 
-    # Crear un DataLoader para facilitar el acceso a los datos en lotes
+    # Crea un data leader per a facilitar l'acces de les dades
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=5, shuffle=True)
 
-
+    # redimensiona les imatges
     train_images_resized = [F.interpolate(img.unsqueeze(0), size=target_size, mode='bilinear', align_corners=False).squeeze(0) for img in train_images]
 
-    # Convertir listas a matrices NumPy
+    # Converteix en llistes les matrius NumPy
     X = torch.stack(train_images_resized).numpy()
     y = np.array(train_labels)
 
-    # Dividir el conjunto de datos en conjuntos de entrenamiento y prueba
+    # 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
     # Redimensionar las matrices para que sean compatibles con el clasificador SVM
