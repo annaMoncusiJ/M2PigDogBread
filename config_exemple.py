d0bcdad47d091b4735779b41cbe2123a067c48a9
aMoncusiJ <anna.moncusi@gmail.com>
2024-01-22 12:25:07 +0100

    Canvi de comentaris i correcció d'error imatge no png

--- config_exemple.py
+++ config_exemple.py
@@ -1,2 +1,1 @@
 url = "C:\\Users\\Francesc\\Downloads\\M4DogPigBread\\"
-url_imatge = "C:\\Users\\Francesc\\Downloads\\M4DogPigBread\\Pig\\descarga.png"
--- interficieGrafica.py
+++ interficieGrafica.py
@@ -5,54 +5,60 @@ from main import calcula_que_es
 import os
 
 def mostrar_imagen(imagen_path, resultado_text):
-    global imagen_tk  # Hacer la referencia global
+    global imagen_tk
 
     imagen = Image.open(imagen_path)
 
-    # Obtén las dimensiones reales del lienzo
+    # Obte les dimencions de la xona on es posa l'imatge
     canvas_width = lienzo.winfo_reqwidth()
     canvas_height = lienzo.winfo_reqheight()
 
-    # Redimensiona la imagen para ajustarla al lienzo
+    # Redimensiona la imagen per ajustarla a l'interficie grafica
     imagen.thumbnail((canvas_width, canvas_height))
     imagen_tk = ImageTk.PhotoImage(imagen)
 
-    # Calcula la posición para centrar la imagen en el lienzo
+    # Calcula la posició per a centrar l'imatge
     x = (canvas_width - imagen_tk.width()) // 2
     y = (canvas_height - imagen_tk.height()) // 2
 
-    # Limpia el lienzo antes de mostrar la nueva imagen
+    # Elimina l'imatge anterior
     lienzo.delete("all")
 
-    # Muestra la imagen en el lienzo
+    # Mostra l'imatge
     lienzo.create_image(x, y, anchor=tk.NW, image=imagen_tk)
 
-    # Actualiza el texto de resultado
+    # Canvia el text amb el nou resultat
     resultado_text.config(text=calcula_que_es(imagen_path))
 
+# crea un event
 def on_drop(event):
+    #agafa les dades de la foto penjada
     archivo = event.data
-    archivo_sin_corchetes = archivo.replace('{', '').replace('}', '')  # Eliminar corchetes
-    archivo_norm = os.path.normpath(archivo_sin_corchetes)  # Normalizar la ruta
+    
+    # ho torna en text pla per a que no dongui errors amb els espais
+    archivo_sin_corchetes = archivo.replace('{', '').replace('}', '')
+    archivo_norm = os.path.normpath(archivo_sin_corchetes)
+
     mostrar_imagen(archivo_norm, resultado)
 
 
-# Crear la ventana principal
+# Crea la finestra principal de l'interficie grafica
 ventana = TkinterDnD.Tk()
 ventana.title("Arrastra y Suelta Imagen")
 
-# Crear un lienzo para mostrar la imagen
+# Crear un "lienzo" par a mostrar l'imatge'
 lienzo = tk.Canvas(ventana, width=400, height=400, bg="white")
 lienzo.pack(pady=10)
 
-# Etiqueta para mostrar el resultado
+# Crea l'etiqueta on es mostrara el text
 resultado = tk.Label(ventana, text="", font=("Helvetica", 12))
 resultado.pack(pady=10)
 resultado.config(text="Arrastra y Suelta Imagen")
 
 
-# Configurar la zona de arrastre (que es el propio lienzo)
+# Configura per a que es puguin arrosegar les imatges
 lienzo.drop_target_register(DND_FILES)
 lienzo.dnd_bind('<<Drop>>', on_drop)
 
+# obra la finestra
 ventana.mainloop()
